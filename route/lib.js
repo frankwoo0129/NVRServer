fedb2a463c665a49b9c6496c256432f035308658
FrankWOO <frankwoo.scc@gmail.com>
2015-01-26 18:47:32 +0800

    (1/26 day) Changes to be committed: 	modified:   .gitignore 	new file:   bower.json 	modified:   lib/executor.js 	modified:   package.json 	modified:   route/lib.js 	modified:   views/example.html

--- route/lib.js
+++ route/lib.js
@@ -3,23 +3,20 @@ var path = require('path');
 var express = require('express');
 
 var root = express.Router();
+/*
 var router_videojs = express.Router();
 var router_videojs_contrib_media_sources = express.Router();
 var router_videojs_contrib_hls = express.Router();
 var router_pkcs7 = express.Router();
 
-router_videojs.use(
-	express.static(path.join(__dirname, '../node_modules/video.js/dist/video-js/')));
-router_videojs_contrib_media_sources.use(
-	express.static(path.join(__dirname, '../node_modules/videojs-contrib-hls/node_modules/videojs-contrib-media-sources/src/')));
-router_videojs_contrib_hls.use(
-	express.static(path.join(__dirname, '../node_modules/videojs-contrib-hls/src/')));
-router_pkcs7.use(
-	express.static(path.join(__dirname, '../node_modules/videojs-contrib-hls/node_modules/pkcs7/dist/')));
-
-root.use('/videojs', router_videojs);
-root.use('/videojs-contrib-media-sources', router_videojs_contrib_media_sources);
-root.use('/videojs-contrib-hls', router_videojs_contrib_hls);
-root.use('/pkcs7', router_pkcs7);
+router_videojs.use(express.static(path.join(__dirname, '../bower_components/video.js/dist/video-js/')));
+router_videojs_contrib_media_sources.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/node_modules/videojs-contrib-media-sources/src/')));
+router_videojs_contrib_hls.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/src/')));
+router_pkcs7.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/node_modules/pkcs7/dist/')));
+*/
+root.use('/videojs', express.Router().use(express.static(path.join(__dirname, '../bower_components/videojs/dist/video-js/'))));
+root.use('/videojs-contrib-media-sources', express.Router().use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-media-sources/src/'))));
+root.use('/videojs-contrib-hls', express.Router().use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/src/'))));
+root.use('/pkcs7', express.Router().use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/node_modules/pkcs7/dist/'))));
 	
 module.exports = root;
