02f43a38fd2c7c2cfc40f2c27d0abb30d413a93f
FrankWOO <frankwoo.scc@gmail.com>
2015-07-13 18:59:43 +0800

    (7/13 day)

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var path = require('path');
 var underscore = require('underscore');
 var mkdirp = require('mkdirp');
+var async = require('async');
 var log = require('loglevel');
 var option = require('./option');
 var model = require('./model');
@@ -13,36 +14,40 @@ log.setLevel(option.loglevel);
 var storage = JSON.parse(fs.readFileSync('./config/' + option.site + '/storage.json', {encoding: 'utf-8'}));
 var config = JSON.parse(fs.readFileSync('./config/' + option.site + '/camera.json', {encoding: 'utf-8'}));
 
+var mk = function (p, callback) {
+	mkdirp(p, function (err) {
+		if (err) {
+			callback(new Error('mkdir ' + p + ' error'));
+		} else {
+			log.debug('mkdir: ' + fs.realpathSync(p));
+			callback();
+		}
+	});
+};
+
 if (!storage.jpegpath) {
 	throw new Error('No jpegpath');
 } else if (!storage.videopath) {
 	throw new Error('No videopath');
 } else {
-	mkdirp(storage.jpegpath, function (err) {
-		if (err) {
-			throw new Error('mkdir jpegpath error');
-		} else {
-			log.debug('mkdir: ' + fs.realpathSync(storage.jpegpath));
+	async.series([
+		function (callback) {
+			async.each([storage.jpegpath, storage.videopath], mk, function (err) {
+				if (err) {
+					throw err;
+				}
+			});
+		}, function (callback) {
+			async.forEachOf(storage.storage, function (value, key, callback) {
+				var p = path.join(storage.videopath, value.path);
+				mk(p, callback);
+			}, function (err) {
+				if (err) {
+					throw err;
+				}
+			});
 		}
-	});
-	
-	mkdirp(storage.videopath, function (err) {
-		if (err) {
-			throw new Error('mkdir videopath error');
-		} else {
-			log.debug('mkdir: ' + fs.realpathSync(storage.videopath));
-		}
-	});
-	
-	underscore.each(storage.storage, function (value, key) {
-		mkdirp(path.join(storage.videopath, value.path), function (err) {
-			if (err) {
-				throw new Error('mkdir videopath error');
-			} else {
-				log.debug('mkdir: ' + fs.realpathSync(path.join(storage.videopath, value.path)));
-			}
-		});
-	});
+	]);
 }
 
 module.exports.config = config;
